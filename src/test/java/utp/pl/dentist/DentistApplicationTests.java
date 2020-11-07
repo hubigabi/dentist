93f45b9b077e1ee11f867279181540ea7f4167ed
hubigabi <hubigabi19@gmail.com>
2020-11-07 06:21:34 +0100

    Init a project

---
+++ src/test/java/utp/pl/dentist/DentistApplicationTests.java
@@ -0,0 +1,13 @@
+package utp.pl.dentist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
